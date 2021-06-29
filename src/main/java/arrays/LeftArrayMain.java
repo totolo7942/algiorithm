38f8f95419c65eab1bd03ac38b9d8603edd8510c
백태성(Taesung Baek)/CX개발팀/11ST <aaron.baek@sk.com>
2021-06-29 13:10:34 +0900

    feat: 보기좋게 변경

--- src/main/java/arrays/LeftArrayMain.java
+++ src/main/java/arrays/LeftArrayMain.java
@@ -7,50 +7,49 @@ import java.util.stream.Collectors;
 
 public class LeftArrayMain {
 
+	//		print , input 5, 4 / result : 5 1 2 3 4
 	public static void main(String[] args) {
-			List<Integer> items = Arrays.asList(1, 2, 3, 4, 5);
-			int d = 4;
+		List<Integer> items = Arrays.asList(1, 2, 3, 4, 5);
+		int d = 4;
+		List<Integer> result1 = rotLeft(items, d);
+		extracted(result1);
 
-//			List<Integer> result = rotLeft(items, d);
-
-		List<Integer> result = rotLeftArrayCopy(items, d);
+		List<Integer> result2 = rotLeftArrayCopy(items, d);
+		extracted(result2);
 
 		int[] ints = rotLeft2(items.stream().mapToInt(i -> i).toArray(), d);
 		System.out.println(Arrays.toString(ints));
 
+	}
 
-//		print , input 5, 4 / result : 5 1 2 3 4
-			String strResult = result.stream().map(String::valueOf).collect(Collectors.joining(","));
-		  System.out.println(strResult);
+	private static void extracted(List<Integer> result) {
+		String strResult = result.stream().map(String::valueOf).collect(Collectors.joining(","));
+		System.out.println(strResult);
 	}
 
 	static List<Integer> rotLeft(List<Integer> a, int d) {
 		List<Integer> l1 = a;
 
 		int tmp = 0;
-		for( int rindex =0; rindex < d; rindex ++ ) {
+		for (int rindex = 0; rindex < d; rindex++) {
 
-				for( int idx =0; idx < a.size(); idx ++ ) {
-					if (idx == 0)
-						tmp = a.get(idx);
-					else
-						l1.set(Math.max(idx - 1, 0), a.get(idx));
-				}
+			for (int idx = 0; idx < a.size(); idx++) {
+				if (idx == 0) { tmp = a.get(idx); } else { l1.set(Math.max(idx - 1, 0), a.get(idx)); }
+			}
 
-				l1.set( a.size() -1, tmp);
+			l1.set(a.size() - 1, tmp);
 		}
-
 
 		return l1;
 	}
 
 	static List<Integer> rotLeftArrayCopy(List<Integer> a, int d) {
 
-		List<Integer> tmp = Arrays.asList(0,0,0,0,0);
+		List<Integer> tmp = Arrays.asList(0, 0, 0, 0, 0);
 		int n = a.size();
 
-		for( int oldIndex = 0; oldIndex < n; oldIndex ++) {
-			int newIndex = (oldIndex + n - d ) % n;
+		for (int oldIndex = 0; oldIndex < n; oldIndex++) {
+			int newIndex = (oldIndex + n - d) % n;
 			tmp.set(newIndex, a.get(oldIndex));
 		}
 
